477c68962983296a49e98e2a34ca31eb329ecd20
onkar <onkarsawant111@gmail.com>
2024-05-29 22:43:19 +0530

    updated

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('home.html', views.home),
+    path('aboutme.html', views.aboutme),
+    path('contactme.html', views.contactme),
+    path('services.html', views.services),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def aboutme(request):
+    return render(request,'aboutme.html')
+def contactme(request):
+    return render(request,'contactme.html')
+def services(request):
+    return render(request,'services.html')
