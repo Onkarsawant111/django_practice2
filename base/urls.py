9ea2ef5343bf428f38ebd85de8ef73a223e69e85
onkar <onkarsawant111@gmail.com>
2024-05-30 23:05:25 +0530

    updated

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path("home/",views.home, name='home'),
-    path("aboutme/", views.aboutme,name="aboutme"),
+    path("about-me/", views.aboutme, name="aboutme"),
     path("services/",views.services, name="services"),
-    path("contactme/", views.contactme, name="contactme"),
+    path("contact-me/", views.contactme, name="contactme"),
 ]
