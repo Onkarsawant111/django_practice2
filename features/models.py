09dfa2fc73ca20647a15ea478ccfbf9c7fef686d
onkar <onkarsawant111@gmail.com>
2024-06-09 14:40:39 +0530

    updated

--- features/models.py
+++ features/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
 class Uses(models.Model):
     uses_title = models.CharField(max_length=30)
-    uses_des = models.CharField(max_length=70)
+    uses_des = models.CharField(max_length=500)
--- features/views.py
+++ features/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from features.models import Uses
+from django.core.paginator import Paginator
 
 # Create your views here.
 def uses(request):
@@ -8,10 +9,13 @@ def uses(request):
         search = request.GET.get("search")
         if search != None:
             data = Uses.objects.filter(uses_title__icontains = search) or Uses.objects.filter(uses_des__icontains = search)
+    
+    pagi = Paginator(data, 2)
+    page_num = request.GET.get('page')
+    pagi_data = pagi.get_page(page_num)
         
-                
-    content = {
-        'uses':data
+    content = { 
+        'uses':pagi_data
     }
     return render(request, 'uses.html', content)
 
